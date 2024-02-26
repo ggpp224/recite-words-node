92f4d9eec06cf2c4fd2876aea220568ac91329fd
guopeng <394850111@qq.com>
2024-02-26 16:44:51 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import {graphqlHTTP} from 'express-graphql';
+import {buildSchema} from "graphql";
+import mysql from 'mysql';
+import {schema} from "./schema";
+
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    insecureAuth : true
+
+});
+
+
+const app = express();
+const port = process.env.PORT||3000;
+
+/*// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    hello: () => {
+        return "Hello world!"
+    },
+}*/
+
+const root = {
+    Words: (obj, args, context, info) => {
+        return new Promise((resolve, reject) => {
+            const criteriaStr = obj.criteriaStr;
+            let sql = `SELECT * FROM words`;
+            if(criteriaStr){
+                sql += ` where ${criteriaStr}`
+            }
+            pool.query(sql, (error, results) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(results);
+            });
+        });
+    },
+};
+
+
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+)
+
+
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
